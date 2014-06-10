9389122253aee3f9389e59247c3f6c96a784e520
Sadhana Sahasrabudhe <sadhanas@yahoo-inc.com>
2014-06-09 21:51:53 -0700

    New chagnes with rating bar and tip split

--- gen/com/ad/example/tipcalculatorapp/R.java
+++ gen/com/ad/example/tipcalculatorapp/R.java
@@ -14,16 +14,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btFifteen=0x7f060002;
-        public static final int btTen=0x7f060001;
-        public static final int btTewnty=0x7f060003;
-        public static final int etAmt=0x7f060000;
-        public static final int tvAmt=0x7f060007;
-        public static final int tvLable1=0x7f060006;
-        public static final int tvLable2=0x7f060004;
-        public static final int tvLable3=0x7f060008;
-        public static final int tvTip=0x7f060005;
-        public static final int tvTotal=0x7f060009;
+        public static final int etAmt=0x7f060001;
+        public static final int rbService=0x7f060006;
+        public static final int sbSplit=0x7f060002;
+        public static final int tvAmount=0x7f060009;
+        public static final int tvAmtVal=0x7f06000b;
+        public static final int tvNumber=0x7f060000;
+        public static final int tvService=0x7f060011;
+        public static final int tvSplit=0x7f060004;
+        public static final int tvSplitVal=0x7f060003;
+        public static final int tvTipPer=0x7f060005;
+        public static final int tvTipPerPerson=0x7f060008;
+        public static final int tvTipPerPersonVal=0x7f06000c;
+        public static final int tvTotalAmount=0x7f06000f;
+        public static final int tvTotalAmountVal=0x7f060010;
+        public static final int tvTotalPerPerson=0x7f06000a;
+        public static final int tvTotalPerPersonVal=0x7f06000e;
+        public static final int tvTotalTip=0x7f060007;
+        public static final int tvTotalTipVal=0x7f06000d;
     }
     public static final class layout {
         public static final int activity_tip_calculator=0x7f030000;
@@ -35,12 +43,28 @@ public final class R {
         public static final int btTwenty=0x7f040005;
         public static final int etAmt=0x7f040002;
         public static final int hello_world=0x7f040001;
+        public static final int tvAmount=0x7f040012;
         public static final int tvAmt=0x7f040009;
+        public static final int tvAmtVal=0x7f040013;
         public static final int tvLable=0x7f040006;
         public static final int tvLable1=0x7f040008;
         public static final int tvLable3=0x7f04000a;
+        public static final int tvNumber=0x7f04000e;
+        public static final int tvService=0x7f04000c;
+        public static final int tvSplit=0x7f04000f;
+        public static final int tvSplitAmong=0x7f04000d;
+        public static final int tvSplitVal=0x7f040010;
         public static final int tvTip=0x7f040007;
+        public static final int tvTipPer=0x7f040011;
+        public static final int tvTipPerPerson=0x7f040014;
+        public static final int tvTipPerPersonVal=0x7f040015;
         public static final int tvTotal=0x7f04000b;
+        public static final int tvTotalAmount=0x7f04001a;
+        public static final int tvTotalAmountVal=0x7f04001b;
+        public static final int tvTotalPerPerson=0x7f040018;
+        public static final int tvTotalPerPersonVal=0x7f040019;
+        public static final int tvTotalTip=0x7f040016;
+        public static final int tvTotalTipVal=0x7f040017;
     }
     public static final class style {
         /** 
--- src/com/ad/example/tipcalculatorapp/TipCalculatorActivity.java
+++ src/com/ad/example/tipcalculatorapp/TipCalculatorActivity.java
@@ -2,10 +2,16 @@ package com.ad.example.tipcalculatorapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,40 +20,135 @@ public class TipCalculatorActivity extends Activity {
 	private TextView tvView;
 	private TextView tvAmt;
 	private TextView tvTotal;
+	private RatingBar rbService;
+	private TextView tvService;
+	private TextView tvTotalTipVal;
+	private SeekBar sbSplit;
+	
+	private TextView tvSplitVal;
+	private TextView tvTipPerPersonVal;
+	private TextView tvAmtVal;
+	private TextView tvTotalPerPersonVal;
+	private TextView tvTotalAmountVal;
+	
+	private TextView tvTipPer;
+	
+	int progressCh =0;
+	int realRating =0;
+
+	
+	float curRating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calculator);
         etAmt = (EditText) findViewById(R.id.etAmt);
-        tvView = (TextView) findViewById(R.id.tvTip);
-        tvAmt = (TextView) findViewById(R.id.tvAmt);
-        tvTotal = (TextView) findViewById(R.id.tvTotal);
+        etAmt.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+		
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				
+				calculateTip();
+			}
+		});
+        
+        rbService = (RatingBar) findViewById(R.id.rbService);
+        tvService = (TextView) findViewById(R.id.tvService);
+        sbSplit = (SeekBar) findViewById(R.id.sbSplit);
+        
+        
+        tvTipPer = (TextView) findViewById(R.id.tvTipPer);
+        tvTotalTipVal = (TextView) findViewById(R.id.tvTotalTipVal);
+        tvSplitVal = (TextView) findViewById(R.id.tvSplitVal);
+        tvTipPerPersonVal = (TextView) findViewById(R.id.tvTipPerPersonVal);
+        
+        tvAmtVal =(TextView) findViewById(R.id.tvAmtVal);
+        tvTotalPerPersonVal = (TextView) findViewById(R.id.tvTotalPerPersonVal);
+        tvTotalAmountVal = (TextView) findViewById(R.id.tvTotalAmountVal);
+        
+        curRating=0;
+        rbService.setRating(curRating);
+        sbSplit.setMax(10);
+        sbSplit.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			//int progressCh =0;
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) { tvSplitVal.setText(String.valueOf(progressCh)); calculateTip(); }
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) { }
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				progressCh = progress;
+				
+				
+				
+			}
+		});
+        
+        addListenerOnRatingBar();
     }
     
-    private Long calculateTip(Long amount, Long i) {
-    	return (amount * i) /100;
-    }
-    public void onClickTen(View v) {
-    	String amt = etAmt.getText().toString();
-    	Long tip = calculateTip(Long.parseLong(amt),10L);
-    	tvView.setText("$ "+String.format("%s",tip));
-    	tvAmt.setText("$ " +amt);
-    	tvTotal.setText("$ "+ (Long.parseLong(amt)+tip));
+    private void calculateTip() {
+    	String amount = etAmt.getText().toString();
     	
+    	Double tipPerPerson =0.0;
+    	Double amountPerPerson =0.0;
+    	
+    	tvAmtVal.setText("$ "+amount);
+    	if(amount.isEmpty()) {
+    		tvTotalTipVal.setText("$0.0"); 
+    		return;
+    	}
+
+    	Double amt = Double.parseDouble(amount);
+    	Double val =0.0;
+    	if(amt!=0.0){
+    		val= (amt * realRating) /100;
+    		tvTotalTipVal.setText("$" + val.toString()); 
+    		if(progressCh !=0){
+    			tipPerPerson = val/progressCh;
+    			amountPerPerson = amt/progressCh;
+    			tvTipPerPersonVal.setText("$ "+tipPerPerson.toString());
+    			tvTotalPerPersonVal.setText("$"+ (tipPerPerson+amountPerPerson));
+    		}else{
+    			tvTipPerPersonVal.setText("$ 0.0");
+    			tvTotalPerPersonVal.setText("$ 0.0");
+    		}
+    		tvTotalAmountVal.setText("$"+(val+amt));
+    	}
+    	return ;
     }
-    public void onClickFifteen(View v) {
-    	String amt = etAmt.getText().toString();
-    	Long tip = calculateTip(Long.parseLong(amt),15L);
-    	System.out.println("What is the tip --->"+tip);
-    	tvView.setText("$ "+String.format("%s",tip));
-    	tvAmt.setText("$ " +amt);
-    	tvTotal.setText("$ "+ (Long.parseLong(amt)+tip));
-    }
-    public void onClickTwenty(View v) {
-    	String amt = etAmt.getText().toString();
-    	Long tip = calculateTip(Long.parseLong(amt),20L);
-    	tvView.setText("$ "+String.format("%s",tip));
-    	tvAmt.setText("$ " +amt);
-    	tvTotal.setText("$ "+ (Long.parseLong(amt)+tip));
+  
+    
+    
+    public void addListenerOnRatingBar() {
+    	rbService.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+    		public void onRatingChanged(RatingBar ratingBar, float rating,  boolean fromUser) {
+    			realRating = (int) rating * 5;
+    			//tvService.setText(String.valueOf(realRating));
+    			String val = String.valueOf(realRating)+" %";
+    			tvTipPer.setText(val);
+    			//rbService.setRating(rating);
+    			calculateTip();
+    			
+    			//Toast.makeText(getApplicationContext(), val, Toast.LENGTH_SHORT).show();
+    		}
+    	});
+    	
+
+    
     }
  }
